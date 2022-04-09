5c249b408a432d7c146a487fdc5b2da02395e799
Nguyễn Thị Linh <libh1004@gmail.com>
2022-04-09 08:48:34 +0700

    create-form

---
+++ SurveyEducation/Models/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurveyEducation.Models
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public int QuestionId { get; set; }
+        public string Position { get; set; }
+        public string Content { get; set; }
+    }
+}

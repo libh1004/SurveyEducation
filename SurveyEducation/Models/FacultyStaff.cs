abe35a6e19032f36a3bd92cc33365c95fe980189
Nguyễn Thị Linh <libh1004@gmail.com>
2022-04-13 10:13:03 +0700

    update

---
+++ SurveyEducation/Models/FacultyStaff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurveyEducation.Models
+{
+    public class FacultyStaff
+    {
+        public string Name { get; set; }
+        public string EmployeeNumber { get; set; }
+        public string Password { get; set; }
+        public string Class { get; set; }
+        public string Specification { get; set; }
+        public string Section { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public virtual ICollection<UserQuestion> UserQuestions { get; set; }
+    }
+}

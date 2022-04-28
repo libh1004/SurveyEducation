d9dc9b61fcdee89abc1084e17ad1df59022b5db9
Nguyễn Thị Linh <libh1004@gmail.com>
2022-04-28 20:02:46 +0700

    update-statistic-survey

---
+++ SurveyEducation/Content/Admin/statistic_survey.js
@@ -0,0 +1,35 @@
+﻿var IndexRender = {
+    render_Question: function (lstObj) {
+        var html = ``;
+        return html;
+    },
+    render_Question_Answer: function (questionType, answers, questionId) {
+        var html = ``;
+        return html;
+    }
+}
+
+
+var Index = function () {
+    /* ------ Methods ------ */
+    var getStatistic = async function (obj) {
+        
+    }
+
+    /* ------ Handles ------ */
+    var handleBox = async function () {
+        $('[data-box="statistic-box"]').map(async function (item) {
+            getStatistic($(item));
+        });
+    }
+
+    return {
+        initialize: async function () {
+            await handleBox();
+        }
+    }
+}();
+
+jQuery(document).ready(function () {
+    Index.initialize();
+});

537cf40f93a2c2af7e2a080d5d21b51a41f8d57c
Nguyễn Thị Linh <libh1004@gmail.com>
2022-05-05 06:48:24 +0700

    update

--- SurveyEducation/Areas/Admin/Controllers/AccountsController.cs
+++ SurveyEducation/Areas/Admin/Controllers/AccountsController.cs
@@ -25,7 +25,7 @@ namespace SurveyEducation.Areas.Admin.Controllers
         public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
 
-            ViewBag.ListAccount = this.db.Accounts.ToList();
+            ViewBag.ListAccount = this.db.Users.ToList();
 
             ViewBag.CurrentSort = sortOrder;
             ViewBag.IdSortParm = sortOrder == "Id" ? "id_desc" : "Id";
@@ -40,7 +40,7 @@ namespace SurveyEducation.Areas.Admin.Controllers
             }
             ViewBag.CurrentFilter = searchString;
 
-            var accounts = from p in db.Accounts
+            var accounts = from p in db.Users
                            select p;
             if (!String.IsNullOrEmpty(searchString))
             {
@@ -66,7 +66,7 @@ namespace SurveyEducation.Areas.Admin.Controllers
         // GET: Accounts
         public ActionResult Index()
         {
-            return View(db.Accounts.ToList());
+            return View(db.Users.ToList());
         }
 
         // GET: Accounts/Details/5
@@ -76,7 +76,7 @@ namespace SurveyEducation.Areas.Admin.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Account account = db.Accounts.Find(id);
+            Account account = db.Users.Find(id);
             if (account == null)
             {
                 return HttpNotFound();
@@ -99,7 +99,7 @@ namespace SurveyEducation.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
-                db.Accounts.Add(account);
+                db.Users.Add(account);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
@@ -114,7 +114,7 @@ namespace SurveyEducation.Areas.Admin.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Account account = db.Accounts.Find(id);
+            Account account = db.Users.Find(id);
             if (account == null)
             {
                 return HttpNotFound();
@@ -145,7 +145,7 @@ namespace SurveyEducation.Areas.Admin.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Account account = db.Accounts.Find(id);
+            Account account = db.Users.Find(id);
             if (account == null)
             {
                 return HttpNotFound();
@@ -158,8 +158,8 @@ namespace SurveyEducation.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Account account = db.Accounts.Find(id);
-            db.Accounts.Remove(account);
+            Account account = db.Users.Find(id);
+            db.Users.Remove(account);
             db.SaveChanges();
             return RedirectToAction("Index");
         }

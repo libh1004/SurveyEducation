743268651b07b89c9414434e031db954da8057fa
hieu130 <hieuvanphan275@gmail.com>
2022-05-05 07:59:28 +0700

    error

--- SurveyEducation/ViewModels/SurveyHistoryViewModel.cs
+++ SurveyEducation/ViewModels/SurveyHistoryViewModel.cs
@@ -12,12 +12,8 @@ namespace SurveyEducation.ViewModels
     {
         [Key]
         public int Id { get; set; }
-        [Key, Column(Order = 1)]
         public int UserId { get; set; }
-        public virtual ICollection<Account> Users { get; set; }
-        [Key, Column(Order = 2)]
         public int SurveyId { get; set; }
-        public virtual ICollection<Survey> Surveys { get; set; }
         public string Answers { get; set; }
         public int Status { get; set; }
     }

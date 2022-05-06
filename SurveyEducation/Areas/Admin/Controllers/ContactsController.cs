3d5e081d93fba337539a0e71779711c9ea6c2b10
Vietcuong0508 <cuongtvth2009009@fpt.edu.vn>
2022-05-06 14:57:26 +0700

    4.7-last-update

---
+++ SurveyEducation/Areas/Admin/Controllers/ContactsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using SurveyEducation.Data;
+using SurveyEducation.Models;
+
+namespace SurveyEducation.Areas.Admin.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private MyDbContext db = new MyDbContext();
+
+        // GET: Admin/Contacts
+        [HttpGet]
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+
+            ViewBag.ListContact = this.db.Contacts.ToList();
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.IdSortParm = sortOrder == "Id" ? "id_desc" : "Id";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            var contacts = from p in db.Contacts
+                        select p;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                contacts = contacts.Where(p => p.Name.Contains(searchString) || p.Email.Contains(searchString) || p.Message.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+
+                case "id_desc":
+                    contacts = contacts.OrderByDescending(p => p.Id);
+                    break;
+                default:
+                    contacts = contacts.OrderBy(p => p.Id);
+                    break;
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(contacts.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Admin/Contacts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // GET: Admin/Contacts/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Contacts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Email,Message")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contact);
+        }
+
+        // GET: Admin/Contacts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Admin/Contacts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Email,Message")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        // GET: Admin/Contacts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        // POST: Admin/Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
